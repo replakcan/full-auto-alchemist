c7c6ff7178f823f0ae3ede0d2ac85c048a9949c2
Alper Akcan <alper.makcan@gmail.com>
2026-04-16 14:38:07 +0300

    backend(refactor): update db connection uri

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
-const uri = process.env.MONGODB_URI + process.env.MONGODB_DB_NAME || 'mongodb://localhost:27017/fallback-db'
+const uri =
+  process.env.MONGODB_URI && process.env.MONGODB_DB_NAME
+    ? `${process.env.MONGODB_URI}${process.env.MONGODB_DB_NAME}`
+    : 'mongodb://localhost:27017/fallback-db'
 
 export async function connectDB() {
   try {
