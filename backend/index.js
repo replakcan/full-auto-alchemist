e4b193a16e06d0f31f78be7e8bc244a133a9bf9f
Alper Akcan <alper.makcan@gmail.com>
2026-04-16 14:43:38 +0300

    feature(backend): add an endpoint for backend healt-check

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import blogRouter from './routes/blog.js'
 import indexRouter from './routes/index.js'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -14,6 +15,13 @@ app.use(express.json())
 
 app.use('/', indexRouter)
 app.use('/blogs', blogRouter)
+
+app.get('/health', (req, res) => {
+  if (mongoose.connection.readyState === 1) {
+    return res.status(200).send('OK')
+  }
+  return res.status(500).send('DB not connected')
+})
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
